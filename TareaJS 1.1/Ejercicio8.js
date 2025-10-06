c56f692254e1783cd1354efaefcaad0e98099b6d
chungo1998prime <jagomezc@unah.hn>
2025-10-06 13:43:30 -0600

    Ejercicio 8

---
+++ TareaJS 1.1/Ejercicio8.js
@@ -0,0 +1,16 @@
+ 
+ function filtrarDisponibles(productos, categoriaBuscada) {
+  return productos.filter(producto => 
+    producto.categoria === categoriaBuscada && producto.stock > 0
+  );
+}
+
+const productos = [
+  { nombre: 'Camisa', categoria: 'ropa', stock: 10 },
+  { nombre: 'Pantalón', categoria: 'ropa', stock: 0 },
+  { nombre: 'Televisor', categoria: 'electrónica', stock: 5 }
+];
+
+const resultado = filtrarDisponibles(productos, 'ropa');
+console.log(resultado);
+
